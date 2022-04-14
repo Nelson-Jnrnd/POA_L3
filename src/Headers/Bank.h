bcc8ebf5deebca8359d32196aec21cd93d9d6870
Androto9 <andre.marquesnora@heig-vd.ch>
2022-04-14 16:27:16 +0200

    init

---
+++ src/Headers/Bank.h
@@ -0,0 +1,10 @@
+#ifndef POA_L3_BANK_H
+#define POA_L3_BANK_H
+
+
+class Bank {
+
+};
+
+
+#endif //POA_L3_BANK_H
---
+++ src/Headers/Boat.h
@@ -0,0 +1,10 @@
+#ifndef POA_L3_BOAT_H
+#define POA_L3_BOAT_H
+
+
+class Boat {
+
+};
+
+
+#endif //POA_L3_BOAT_H
