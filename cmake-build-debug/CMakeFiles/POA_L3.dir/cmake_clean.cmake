c598d04265914c374c7f2867773ed69fd96c391f
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2022-04-14 15:33:28 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/POA_L3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POA_L3.dir/main.cpp.obj"
+  "POA_L3.exe"
+  "POA_L3.exe.manifest"
+  "POA_L3.pdb"
+  "libPOA_L3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POA_L3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
